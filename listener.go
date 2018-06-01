d09e2ed1634d266cddf89e18841aa36ea3422e24
Aliaksandr_Zahorski <aliaksandr_zahorski@epam.com>
2018-06-01 10:13:10 +0300

    working stage

--- listener.go
+++ listener.go
@@ -12,11 +12,8 @@ import (
 
 var (
 	mod = windows.NewLazyDLL("user32.dll")
-)
-
-type (
-	HANDLE uintptr
-	HWND   HANDLE
+	procKeyBd = mod.NewProc("keybd_event")
+	procSendMessage = mod.NewProc("SendMessageW")
 )
 
 const (
@@ -24,6 +21,27 @@ const (
 	ModCtrl
 	ModShift
 	ModWin
+
+	_KEYEVENTF_KEYUP       = 0x0002
+
+	WM_KEYUP               = 0x0101
+	WM_KEYDOWN             = 0x0100
+	WM_CHAR                = 0x0102
+	
+	VK_F1                  = 0x70
+	VK_F2                  = 0x71
+	VK_F3                  = 0x72
+	VK_F4                  = 0x73
+	VK_F5                  = 0x74
+	VK_F6                  = 0x75
+	VK_F7                  = 0x76
+	VK_F8                  = 0x77
+	VK_F9                  = 0x78
+	VK_F10                 = 0x79
+	VK_F11                 = 0x7A
+	VK_F12                 = 0x7B
+
+	VK_A                   = 0x41
 )
 
 type Hotkey struct {
@@ -89,6 +107,11 @@ func main() {
 			// hwnd of current window
 			if hwnd := getWindow("GetForegroundWindow"); hwnd != 0 {
 				fmt.Println("# hwnd:", hwnd)
+				time.Sleep(time.Millisecond * 1000)
+				fmt.Println("sending:", SendMessage(hwnd, VK_A, 0, 0))
+				SendMessage(hwnd, WM_KEYDOWN, VK_F1, 0)
+				time.Sleep(time.Millisecond * 50)
+				SendMessage(hwnd, WM_KEYUP, VK_F1, 0)
 			}
 
 			if id == 3 { // CTRL+ALT+X = Exit
@@ -120,4 +143,14 @@ func getWindow(funcName string) uintptr {
 	proc := mod.NewProc(funcName)
 	hwnd, _, _ := proc.Call()
 	return hwnd
+}
+
+func SendMessage(hwnd uintptr, msg uint32, wParam, lParam uintptr) uintptr {
+	ret, _, _ := procSendMessage.Call(
+		uintptr(hwnd),
+		uintptr(msg),
+		wParam,
+		lParam)
+
+	return ret
 }
